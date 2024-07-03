03ccb0270006ea8a49495280bc4198830333d206
dlaller <dlaller1988@gmail.com>
2024-07-03 15:11:20 +0530

    Initial Backend and Database setup

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+// dotenv.config({ path: '.env.local' });
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+
+console.log(process.env.NODE_ENV);
+
+import express, { Request, Response } from "express";
+import sequelize from "./util/dbConn"
+import cors from "cors";
+
+import errorMiddleware from "./middleware/error";
+import setInterface from "./middleware/interface";
+
+
+const app = express();
+
+app.use(express.json({ limit: '2450mb' }));
+
+app.use(express.urlencoded({ extended: true }));
+var corsOptions = {
+  origin: function (origin: any, callback: any) {
+    callback(null, true);
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(setInterface);
+//check connection to database
+const connectToDb = async () => {
+  const data = await sequelize.sync({ force: false })
+  try {
+    await sequelize.authenticate();
+      console.log("Database Connected successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+
+
+app.use(errorMiddleware);
+
+app.listen(5000, () => {
+  connectToDb();
+  console.log(`[*] Server listening on Port ${5000}`);
+});
