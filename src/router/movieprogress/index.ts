c5c5a035a97deb41a97a98fa9e99b1d2a9a75083
Dinesh <dlaller1988@gmail.com>
2024-11-04 19:30:43 +0530

    Updated Code 30-10-2024

--- src/router/movieprogress/index.ts
+++ src/router/movieprogress/index.ts
@@ -1,9 +1,10 @@
-import {deleteContinue, getContinue, postContinue,} from "../../controllers/movieprogress";
+import {deleteContinue, getContinue, getContinueFortv, postContinue,} from "../../controllers/movieprogress";
 import { Router } from "express";
 
 const router = Router();
 // router.use(accessControl)
 router.get("/", getContinue);
+router.get("/tv", getContinueFortv);
 router.post('/', postContinue);
 router.delete('/', deleteContinue);
 
